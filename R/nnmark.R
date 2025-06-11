62355eb2cbadb350719eb00aa6c0bb7b899655c0
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2025-06-11 11:23:45 +0800

    Extension to nnmark

--- R/nnmark.R
+++ R/nnmark.R
@@ -1,30 +1,67 @@
 #
 # nnmark.R
 #
-# $Revision: 1.7 $ $Date: 2018/02/14 08:00:59 $
+# $Revision: 1.8 $ $Date: 2025/06/11 02:33:12 $
 
 nnmark <- local({
 
-  nnmark <- function(X, ..., k=1, at=c("pixels", "points")) {
+  nnmark <- function(X, ..., k=1, at=c("pixels", "points"),
+                     ties=c("first", "mean", "min", "max"),
+                     distinct=FALSE) {
     stopifnot(is.ppp(X))
     stopifnot(is.marked(X))
     at <- match.arg(at)
+    ties <- match.arg(ties)
     mX <- marks(X)
+    if(ties != "first" && anyDuplicated(P <- unmark(X))) {
+      ## pool marks of coincident points
+      um <- uniquemap(P)
+      uniq <- (um == seq_along(um))
+      poolid <- cumsum(uniq)
+      NX <- coerce.marks.numeric(X)
+      mX <- marks(NX)
+      smX <- split(as.data.frame(mX), factor(um))
+      pmX <- switch(ties,
+                    mean = { lapply(smX, colMeans) },
+                    max =  {
+                      lapply(smX, function(z) { apply(z, 2, max) })
+                    },
+                    min =  {
+                      lapply(smX, function(z) { apply(z, 2, min) })
+                    })
+      pmX <- do.call(rbind, unname(pmX))
+      ## reassign to original pattern
+      mX <- marksubset(pmX, poolid[um])
+    }
     switch(at,
            pixels = {
              Y <- nnmap(X, k=k, what="which", ...)
-             switch(markformat(X),
+             switch(markformat(mX),
                     vector={
                       result <- eval.im(mX[Y])
                     },
                     dataframe = {
                       mX <- as.list(as.data.frame(mX))
                       result <- solapply(mX, lookedup, indeximage=Y)
+                      if(length(result) == 1)
+                        result <- result[[1]]
                     },
                     stop("Marks must be a vector or dataframe"))
            },
            points = {
-             Y <- nnwhich(X, k=k)
+             if(!distinct) {
+               Y <- nnwhich(X, k=k)
+             } else {
+               ## find distinct points
+               P <- unmark(X)
+               um <- uniquemap(P)
+               uid <- which(um == seq_along(um))
+               U <- P[uid]
+               ## find neighbours 
+               Z <- nnwhich(U, k=k)
+               ## map back
+               Y <- uid[Z[um]]
+             }
              switch(markformat(X),
                     vector={
                       result <- mX[Y]
