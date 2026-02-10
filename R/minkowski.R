6a7242969354ed9f17aae2d3aadd16d083c456a7
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2026-02-10 12:36:31 +0800

    Bug fix in MinkowskiSum

--- R/minkowski.R
+++ R/minkowski.R
@@ -3,29 +3,92 @@
 #' 
 #'  Minkowski Sum and related operations
 #'
-#'  $Revision: 1.8 $ $Date: 2020/06/11 01:03:54 $
+#'  $Revision: 1.15 $ $Date: 2026/02/10 03:58:59 $
 
 
 "%(+)%" <- MinkowskiSum <- local({
 
   MinkowskiSum <- function(A, B) {
-    if(is.ppp(A)) return(UnionOfShifts(B, A))
-    if(is.ppp(B)) return(UnionOfShifts(A, B))
-    ## extract lists of simply-connected polygons
-    AA <- simplepolygons(A)
-    BB <- simplepolygons(B)
-    ## determine common resolution for polyclip operations
-    eps <- mean(c(sidelengths(Frame(A)), sidelengths(Frame(B))))/2^30
-    p <- list(eps=eps)
-    ## compute Minkowski sums of simply-connected pieces
-    result <- NULL
-    for(a in AA) {
-      partial.a <- NULL
-      for(b in BB) {
-        contrib.ab <- polyclip::polyminkowski(a, b, x0=0, y0=0, eps=eps)
-        partial.a <- union.owin(partial.a, poly2owin(contrib.ab), p=p)
+    if(is.ppp(A)) {
+      ## point pattern + something
+      result <- UnionOfShifts(B, A)
+    } else if(is.ppp(B)) {
+      ## something + point pattern
+      result <- UnionOfShifts(A, B)
+    } else if(is.psp(A) && is.psp(B)) {
+      ## segments + segments
+      result <- UnionOfParallelograms(A,B)
+    } else if(is.rectangle(A) && is.rectangle(B)) {
+      result <- SumOfBoxes(A,B)
+    } else {
+      ## convert each object to a list of CONVEX polygons
+      if(is.psp(A)) {
+        AA <- psp2poly(A)
+        simpleA <- TRUE
+      } else {
+        A <- as.polygonal(A)
+        if(is.convex(A)) {
+          AA <- list(A)
+          simpleA <- FALSE
+        } else {
+          AA <- tiles(triangulate.owin(A))
+          simpleA <- TRUE
+        }
+        AA <- lapply(AA, owin2poly)
       }
-      result <- union.owin(result, partial.a, p=p)
+      if(is.psp(B)) {
+        BB <- psp2poly(B)
+        simpleB <- TRUE
+      } else {
+        B <- as.polygonal(B)
+        if(is.convex(B)) {
+          BB <- list(B)
+          simpleB <- FALSE
+        } else {
+          BB <- tiles(triangulate.owin(B))
+          simpleB <- TRUE
+        }
+        BB <- lapply(BB, owin2poly)
+      }
+      ## determine common resolution for polyclip operations
+      FRAME <- SumOfBoxes(A, B)
+      x0 <- FRAME$xrange[1L]
+      y0 <- FRAME$yrange[1L]
+      eps <- mean(c(sidelengths(Frame(A)), sidelengths(Frame(B))))/2^30
+      p <- list(eps=eps, x0=x0, y0=y0)
+      ## compute Minkowski sums of pairs of CONVEX pieces
+      result <- NULL
+      if(simpleA && simpleB) {
+        ## each piece is a triangle or line segment; evaluate directly
+        for(a in AA) {
+          partial.a <- NULL
+          for(b in BB) {
+            x <- as.numeric(outer(a$x, b$x, "+"))
+            y <- as.numeric(outer(a$y, b$y, "+"))
+            h <- rev(chull(x,y))
+            contrib.ab <- owin(poly=list(x=x[h], y=y[h]), check=FALSE) 
+            partial.a <- union.owin(partial.a, contrib.ab, p=p)
+          }
+          result <- union.owin(result, partial.a, p=p)
+        }
+      } else {
+        ## general case - use polyclip
+        for(a in AA) {
+          partial.a <- NULL
+          for(b in BB) {
+            contrib.ab <- polyclip::polyminkowski(a, b, x0=x0, y0=y0, eps=eps)
+            contrib.ba <- polyclip::polyminkowski(b, a, x0=x0, y0=y0, eps=eps)
+            ## these must be convex
+            contrib.ab <- fillholes.owin(poly2owin(contrib.ab), Inf)
+            contrib.ba <- fillholes.owin(poly2owin(contrib.ba), Inf)
+            ## union
+            partial.a <- union.owin(partial.a, contrib.ab, contrib.ba, p=p)
+          }
+          result <- union.owin(result, partial.a, p=p)
+        }
+      }
+      result <- rescue.rectangle(result)
+      Frame(result) <- FRAME
     }
     ## resolve unitname
     un <- list(unitname(A), unitname(B))
@@ -35,18 +98,12 @@
     return(result)
   }
 
+  ## processing helper functions
+
   poly2owin <- function(z) owin(poly=z, check=FALSE)
 
-  simplepolygons <- function(A) {
-    if(is.psp(A)) return(psp2poly(A))
-    ## convert to owin, then polygonal
-    A <- as.polygonal(A)
-    ## separate into simply-connected pieces
-    AA <- break.holes(A)$bdry
-    return(AA)
-  }
+  owin2poly <- function(w) { w$bdry }
   
-  ## handle segment patterns as well 
   psp2poly <- function(X) apply(as.matrix(X$ends), 1, seg2poly)
 
   seg2poly <- function(z) with(as.list(z), list(x=c(x0, x1, x0), y=c(y0,y1,y0)))
@@ -70,6 +127,36 @@
     return(Z)
   }
 
+  UnionOfParallelograms <- function(A,B) {
+    ## A and B are segment patterns
+    eA <- as.matrix(A$ends)
+    eB <- as.matrix(B$ends)
+    ## determine common resolution for polyclip operations
+    eps <- mean(c(sidelengths(Frame(A)), sidelengths(Frame(B))))/2^30
+    p <- list(eps=eps)
+    ## form the Minkowski sum of each pair of segments
+    result <- NULL
+    for(i in seq_len(nrow(eA))) {
+      for(j in seq_len(nrow(eB))) {
+        x <- as.numeric(outer(eA[i, c(1,3)], eB[j, c(1,3)], "+"))
+        y <- as.numeric(outer(eA[i, c(2,4)], eB[j, c(2,4)], "+"))
+        h <- rev(chull(x,y))
+        g <- owin(poly=list(x=x[h], y=y[h]), check=FALSE) 
+        result <- union.owin(result, g, p=p)
+      }
+    }
+    return(result)
+  }
+
+  SumOfBoxes <- function(A, B) {
+    A <- Frame(A)
+    B <- Frame(B)
+    xr <- A$xrange + B$xrange
+    yr <- A$yrange + B$yrange
+    result <- owin(xr, yr)
+    return(result)
+  }
+  
   MinkowskiSum
 })
 
